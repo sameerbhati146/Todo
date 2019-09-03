4f47469fdee3b886b9c1db08c3e8112db63f5f08
Sameer Bhati <sameerbhati@Sameers-MacBook-Pro.local>
2019-09-02 23:30:21 -0700

    Getting a response from the server

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -9,13 +10,49 @@ import { ActivatedRoute } from '@angular/router';
 export class WelcomeComponent implements OnInit {
 
   message = "welcome to the ToDo application"
+  welcomeMessageFromService: string
+  errorMessageFromService: string
   name = ''
+  show: boolean = false;
 
-  constructor(private route:ActivatedRoute) { }
+  constructor(private route:ActivatedRoute, private service: WelcomeDataService) { }
 
   ngOnInit() {
     console.log(this.route.snapshot.params['name']);
     this.name = this.route.snapshot.params['name'];
   }
+
+  getWelcomeMessage(){
+    console.log(this.service.executeWelcomeDataService());
+    this.service.executeWelcomeDataService().subscribe(
+      response => this.handlingSuccessfulResponse(response),
+      error => this.handleErrorMessage(error)
+    );
+  }
+
+  getWelcomeMessageWithParameter(name){
+    console.log(this.service.executeWelcomePathVariable(this.name));
+    this.service.executeWelcomePathVariable(this.name).subscribe(
+      response => this.handlingSuccessfulResponse(response),
+      error => this.handleErrorMessage(error)
+    );
+  }
+
+  
+
+  handlingSuccessfulResponse(response){
+    this.welcomeMessageFromService = response.message;
+    //console.log(response.message);
+  }
+
+  handleErrorMessage(error){
+    console.log(error.error.message);
+    this.welcomeMessageFromService = error.error.message;
+    if(this.errorMessageFromService = error.error.message){
+      this.show = true;
+    }this.show = false;
+  }
+
+
 
 }
